64c9d82162caeb5865f577b008ddcc2c2490ebce
acbukka <thesweylis@gmail.com>
2022-08-17 16:42:03 +0700

    feat: make the second form block and implement card inputs

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const linkItems = document.querySelectorAll('.tapir-item');
+const inputBox = document.querySelector(".card__digits");
 
 linkItems.forEach((link) => {
   link.addEventListener('click', () => {
@@ -11,3 +12,15 @@ linkItems.forEach((link) => {
     console.log('bam');
   })
 })
+
+// Добавим слушатель на инпуты в card__digits и настроим правила ввода, чтобы не принималось ничего кроме цифр
+// Также добавим переключение инпута ввода при полном заполнении поля инпута
+inputBox.addEventListener("input", function(e){
+  if (e.target.value.length > 4) {
+    e.target.value = e.target.value.slice(0, 4);
+    if ( e.target.value !== "" && e.target.nextElementSibling && e.target.nextElementSibling.nodeName === "INPUT" ){
+      e.target.nextElementSibling.focus();
+    }
+  }
+});
+
