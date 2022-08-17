19a72dd677a91314019fadc80bc3b1214deda353
acbukka <thesweylis@gmail.com>
2022-08-17 19:50:01 +0700

    feat: add submit button and events for inputs

--- main.js
+++ main.js
@@ -2,9 +2,10 @@
 
 const linkItems = document.querySelectorAll('.tapir-item');
 const inputBox = document.querySelector(".card__digits");
-const cardInputs = document.querySelectorAll('.card__digits-input');
 const cardCVC = document.querySelector('.card__cvc-input');
-let cardsInputsValue = 0;
+const cardOwnerInput = document.querySelector('.card__owner-input');
+const cardInputs = document.querySelectorAll('.card__digits-input');
+
 
 linkItems.forEach((link) => {
   link.addEventListener('click', () => {
@@ -19,21 +20,51 @@ linkItems.forEach((link) => {
 // Добавим слушатель на инпуты в card__digits и настроим правила ввода, чтобы не принималось ничего кроме цифр
 // Также добавим переключение инпута ввода при полном заполнении поля инпута
 inputBox.addEventListener("input", function(e){
-  if (e.target.value.length > 4) {
-    e.target.value = e.target.value.slice(0, 4);
-    if ( e.target.value !== "" && e.target.nextElementSibling && e.target.nextElementSibling.nodeName === "INPUT" ){
-      e.target.nextElementSibling.focus();
+  let cardsInputsValue = '';
+  let cardInput = e.target;
+  if (cardInput.value.length > 4) {
+    cardInput.value = cardInput.value.slice(0, 4);
+    if ( cardInput.value !== "" && cardInput.nextElementSibling && cardInput.nextElementSibling.nodeName === "INPUT" ){
+      cardInput.nextElementSibling.focus();
     }
+  }
+  // будем обновлять счетчик каждый раз когда вводится инпут и вводить в него новые значения
+  cardInputs.forEach((input) => {
+    cardsInputsValue += input.value;
+  })
+  // если счетчик не будет равен 16 знакам - подсвечиваем инпут красным
+  if (cardsInputsValue.length < 16) {
+    cardInputs.forEach((input) => {
+      input.style.background = '#ff5047';
+    })
+  } else {
+    cardInputs.forEach((input) => {
+      input.style.background = '#F0F0F0';
+    })
   }
 });
 
+// Аналогично поступим с CVC, обрезав инпут до 3 символов и выводим подсветку если символов меньше чем 3
 cardCVC.addEventListener("input", function (e){
-  if (e.target.value.length > 3) {
+  if (e.target.value.length >= 3) {
     e.target.value = e.target.value.slice(0, 3);
+    cardCVC.style.background = '#F0F0F0';
+  } else {
+    cardCVC.style.background = '#ff5047';
   }
 })
 
-// cardInputs.forEach((input) => {
-//   cardsInputsValue += input.value;
-// })
-// console.log(cardsInputsValue);
+// Добавим обработку инпута владельца карты (латиница, минимальное кол-во символов и подсветку если символов меньше чем 4)
+cardOwnerInput.addEventListener('input', function (e) {
+  e.target.value = e.target.value.replace(/[^A-Za-z\s]/g,'');
+  if (e.target.value.length < 4) {
+    cardOwnerInput.style.background = '#ff5047';
+  } else {
+    cardOwnerInput.style.background = '#F0F0F0';
+  }
+})
+
+const submitData = function () {
+
+}
+
